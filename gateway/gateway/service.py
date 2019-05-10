47ac50a0e1b5506c1770fdee6fd60db3027b7103
JJINDAHOUSE <jl4574@columbia.edu>
2019-05-09 20:15:17 -0700

    Done with service code

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -96,3 +96,79 @@ class GatewayService(object):
 
             # Retrieve all products from the products service
             product_map = {prod['id']: prod for prod in self.products_rpc.list()}
+
+            # get the configured image root
+            image_root = self.config['PRODUCT_IMAGE_ROOT']
+
+            # Enhance order details with product and image details.
+            for item in order['order_details']:
+                product_id = item ['product_id']
+
+                item['product'] = product_map[product_id]
+                # Construct an image url.
+                item['image'] = '{}/{}.jpg'.format(image_root, product_id)
+
+            return order
+
+            @http(
+                "POST", "/orders",
+                expected_exceptions=(ValidationError, ProductNotFound, BadRequest)
+            )
+            def create_prder(self, request):
+                """Create a new order - order data is posted as json
+
+                Example request ::
+
+                    {
+                        "order_details": [
+                            {
+                                "product_id": "the_odyssey",
+                                "price": "99.99",
+                                "quantity": 1
+                            },
+                            {
+                                "price": "5.99",
+                                "product_id": "the_enigma",
+                                "quantity": 2
+                            },
+                        ]
+                    }
+
+
+                The response contains the new order ID in a json document ::
+
+                    {"id": 1234}
+
+                """
+
+                schema = CreateOrderSchema(strict=True)
+
+                try:
+                    # load input data through a schema (for validation)
+                    # Note - this may raise `ValueError` for invalid json,
+                    # or `validationError` if data is invalid.
+                    order_data = schema.loads(request.get_data(as_text=True)).data
+                except ValueError as exc:
+                    raise BadRequest("Invalid json: {}".format(exc))
+
+                # Create the order
+                # Note - this may raise `ProductNotFound`
+                id_ = self._create_order(order_data)
+                return Response(json.dumps({'id': id_}), mimetype='application/json')
+
+                def _create_order(self, order_data):
+                    # check order product ids are valid
+                    valid_product_ids = {prod['id'] for prod in self.products_rpc.list()}
+                    for item in order_data['order_details']:
+                        if item['product_id'] not in valid_product_ids:
+                            raise ProductNotFound(
+                                "Product Id {}".format(item['product_id'])
+                            )
+                    # Call orders-service to create the order.
+                    # Dump the data through the schema to ensure the values are serialized
+                    # correctly.
+                    serialized_data = CreateOrderSchema().dump(order_data).data
+                    result = self.orders_rpc.create_order(
+                        serialized_data['order_details']
+                    )
+                    return result['id']
